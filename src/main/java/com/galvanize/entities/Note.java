e803c7615562620a4c849eaecde4d0a47dd420a8
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-17 17:05:07 -0500

    added getters, equals, and hash code to Note

--- src/main/java/com/galvanize/entities/Note.java
+++ src/main/java/com/galvanize/entities/Note.java
@@ -2,6 +2,7 @@ package com.galvanize.entities;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Note {
     Timestamp timeOfNote;
@@ -17,5 +18,27 @@ public class Note {
     public Note(Timestamp timeOfNote, String noteDescription) {
         this.timeOfNote = timeOfNote;
         this.noteDescription = noteDescription;
+    }
+
+    public Timestamp getTimeOfNote() {
+        return timeOfNote;
+    }
+
+    public String getNoteDescription() {
+        return noteDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return Objects.equals(getTimeOfNote(), note.getTimeOfNote()) &&
+                Objects.equals(getNoteDescription(), note.getNoteDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTimeOfNote(), getNoteDescription());
     }
 }
