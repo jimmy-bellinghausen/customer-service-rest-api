2be2891e2ad3ab79a0f325b8246839f04620b0fa
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-18 17:47:39 -0500

    added post functionality at endpoint "/api/service"

--- src/main/java/com/galvanize/controllers/RestController.java
+++ src/main/java/com/galvanize/controllers/RestController.java
@@ -4,23 +4,25 @@ import com.galvanize.entities.CustomerRequest;
 import com.galvanize.repositories.JdbcCustomerDao;
 import com.galvanize.repositories.JpaCustomerDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-@org.springframework.web.bind.annotation.RestController("/api/service")
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
-   // @Autowired
- //   JpaCustomerDao jpaCustomerDao;
+    @Autowired
+    JpaCustomerDao jpaCustomerDao;
 
     @Autowired
     JdbcCustomerDao jdbcCustomerDao;
 
-//    @PostMapping
-//    public CustomerRequest postCustomerRequest(@RequestBody CustomerRequest customerRequest){
-//        return new CustomerRequest(Timestamp.valueOf(LocalDateTime.now()), "Test Customer", "123 Some Address Lane", "+1 234-567-890", "This is a test customer request.");
-//
-//    }
+    @PostMapping("/api/service")
+    public CustomerRequest postCustomerRequest(@RequestBody CustomerRequest customerRequest){
+        return jpaCustomerDao.save(customerRequest);
+    }
+
+
 }
--- src/test/java/com/galvanize/controllers/RestControllerTest.java
+++ src/test/java/com/galvanize/controllers/RestControllerTest.java
@@ -1,4 +1,32 @@
 package com.galvanize.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class RestControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void postCustomer() throws Exception {
+        String input = "{\"customerName\":\"Some Customer\",\"customerAddress\":\"123 Any Street, SomeCity, ST, 99999\",\"phoneNumber\":\"111-222-3333\",\"description\":\"it's broke and I need it fixed!\"}";
+        mvc.perform(post("/api/service/").content(input).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.customerName").value("Some Customer"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 }
